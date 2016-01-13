f0a5b0d39796f18920dfa706d922b928d8a51fde
Coranna Howard <me@komiga.com>
2016-01-13 01:10:04 -0500

    pickle: URC (site-core).

--- pickle.lua
+++ pickle.lua
@@ -11,24 +11,24 @@ P.configure{
 	build_path = "build/",
 }
 
-Core.setup_site{
-	title = "komiga",
-	url = "http://komiga.com",
-	run_years = "2012–2016",
-	md_image = "http://lh4.googleusercontent.com/-fek5nINIJug/UMVJdFtlHVI/AAAAAAAAAHw/sb0KGgMaPPU/k-nrm-256.png",
+Core.setup_site(function()
+	title = "komiga"
+	url = "http://komiga.com"
+	run_years = "2012–2016"
+	md_image = "http://lh4.googleusercontent.com/-fek5nINIJug/UMVJdFtlHVI/AAAAAAAAAHw/sb0KGgMaPPU/k-nrm-256.png"
 
-	analytics_id = "UA-6565507-1",
-	analytics_domain_name = nil,
-	human_date_format = "%d %B %Y",
+	analytics_id = "UA-6565507-1"
+	analytics_domain_name = nil
+	human_date_format = "%d %B %Y"
 
 	nav = {
-		home     = NavItem(nil        , "top"      , "#", "komiga.svg", "home"),
-		backhome = NavItem(nil        , "main page", "/", "komiga.svg", "home"),
-		email    = NavItem("@"        , "email"    , "mailto:me@komiga.com"),
-		github   = NavItem(nil        , "GitHub"   , "https://github.com/komiga" , "github.svg"),
-		twitter  = NavItem(nil        , "Twitter"  , "https://twitter.com/komiga", "twitter.svg"),
-	},
-}
+		home     = NavItem(nil, "top"      , "#", "komiga.svg", "home"),
+		backhome = NavItem(nil, "main page", "/", "komiga.svg", "home"),
+		email    = NavItem("@", "email"    , "mailto:me@komiga.com"),
+		github   = NavItem(nil, "GitHub"   , "https://github.com/komiga" , "github.svg"),
+		twitter  = NavItem(nil, "Twitter"  , "https://twitter.com/komiga", "twitter.svg"),
+	}
+end)
 
 P.filter("static", F.copy)
 P.filter("layout", Layout)
