1a929c993a8a8ca476a9f5a1807873e2bf002426
Coranna Howard <me@komiga.com>
2016-01-16 22:46:16 -0500

    src/Section: reworked; added sub() and item().

--- src/Section.lua
+++ src/Section.lua
@@ -7,32 +7,41 @@ local NavItem = require "core/NavItem"
 local M = U.module("Section")
 
 local tpl_url = [[<a target="_blank" href="%s">%s</a>]]
-local tpl_content = [[<h%d>%s<a class="para-ref" id="%s" href="#%s"></a></h%d>]]
+local tpl_ref = [[<a class="para-ref" id="%s" href="#%s"></a>]]
+local tpl_content = [[<h%d>%s%s</h%d>]]
 
-local function make(nav, page, name, url, id, level)
+local function make(page, name, text, url, id, level, add_nav)
 	U.type_assert(page, Page)
 	U.type_assert(name, "string")
+	U.type_assert(text, "string")
 	U.type_assert(url, "string", true)
-	U.type_assert(id, "string", true)
+	U.type_assert_any(id, {"boolean", "string"}, true)
 	level = U.optional(U.type_assert(level, "number", true), 1)
+	U.type_assert(add_nav, "boolean", true)
 
-	id = id or Core.slugize(name)
-	local text
-	if nav then
-		text = "§ " .. name
-		table.insert(page.nav, NavItem(text, nil, "#" .. id))
-	else
-		text = url and string.format(tpl_url, url, name) or name
+	if not U.is_type(id, "boolean") then
+		id = id or Core.slugize(name)
 	end
-	return string.format(tpl_content, level, text, id, id, level)
+	if url then
+		text = string.format(tpl_url, url, text)
+	end
+	if add_nav then
+		table.insert(page.nav, NavItem(text, nil, "#" .. id))
+	end
+	local content = id and string.format(tpl_ref, id, id) or ""
+	return string.format(tpl_content, level, text, content, level)
 end
 
-function M.nav(page, name, url, id, level)
-	return make(true, page, name, url, id, level)
+function M.nav(page, name, id, level)
+	return make(page, name, "§ " .. name, nil, id, level, true)
+end
+
+function M.sub(page, name, url, id, level)
+	return make(page, name, "∗ " .. name, url, id, level or 4)
 end
 
 function M.item(page, name, url, id, level)
-	return make(false, page, name, url, id, level or 4)
+	return make(page, name, name, url, id, level or 4)
 end
 
 return M
